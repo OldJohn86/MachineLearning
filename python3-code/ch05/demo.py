471f173eee9187c1ca9983bc413cf08d2f3b52ee
John.Chen <johnchen5372@gmail.com>
2019-09-19 21:31:24 +0800

    update ch05/demo.py

--- python3-code/ch05/demo.py
+++ python3-code/ch05/demo.py
@@ -37,7 +37,7 @@ def plotBestFit(weights):
     xcord1 = []
     ycord1 = []
     xcord2 = []
-    xcord2 = []
+    ycord2 = []
     for i in range(n):
         if int(labelMat[i]) == 1:
             xcord1.append(dataArr[i, 1])
@@ -51,7 +51,22 @@ def plotBestFit(weights):
     ax.scatter(xcord2, ycord2, s=30, c='green')
     x = arange(-3.0, 3.0, 0.1)
     y = (-weights[0]-weights[1]*x)/weights[2]
-    ax.plt(x, y)
+    ax.plot(x, y)
     plt.xlabel('X1')
     plt.ylabel('X2')
     plt.show()
+
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+    m,n = shape(dataMatrix)
+    weights = ones(n)
+    for j in range(numIter):
+        dataIndex = list(range(m))
+        for i in range(m):
+            alpha = 4/(1.0+j+i)+0.01
+            randIndex = int(random.uniform(0,len(dataIndex)))
+            h = sigmoid(sum(dataMatrix[randIndex]*weights))
+            error = classLabels[randIndex] - h
+            weights = weights + alpha * error * dataMatrix[randIndex]
+            del(dataIndex[randIndex])
+    return weights
+
